198af7139bfad91e88265091ec9ab387aca5d850
Jop Kort <154099962+BagofDoritos69@users.noreply.github.com>
2023-12-18 17:56:11 +0100

    decrypt.php

--- decrypt.php
+++ decrypt.php
@@ -14,6 +14,15 @@ $xor = new XORCipher();
 $cipher = $_SESSION['cipher'];
 echo "The made cipher in the previous tab:<br>$cipher<br>";
 
+// Zorgt ervoor dat elke gegeven error (niet bestaande array) niet verschijnt op beeld
+set_error_handler(function(int $errno, string $errstr) {
+    if ((strpos($errstr, 'Undefined array key') === false) && (strpos($errstr, 'Undefined variable') === false)) {
+        return false;
+    } else {
+        return true;
+    }
+}, E_WARNING);
+
 // Probeert de key te kraken dmv frequentie analise 
 $cracked_key = $xor->crack($cipher, 8);
 
@@ -21,7 +30,7 @@ $cracked_key = $xor->crack($cipher, 8);
 $cracked_plaintext = $xor->plaintext($cipher, $cracked_key);
 
 // Als het is gelukt stelt de text op
-echo "The cracked key should be:<br>".$cracked_key."<br><br>";
+echo "<br>The cracked key should be:<br>".$cracked_key."<br><br>";
 echo "The cracked text should be:<br>".$cracked_plaintext;
 
 // Link terug naar encryptie
